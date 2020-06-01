137479b6753314cdfac9c59a9be01f5fefeeaa35
Mohith Kanthamneni <50568915+Mohith1@users.noreply.github.com>
2020-06-01 10:28:31 +0530

    Update fibonacci series.py

--- fibonacci series.py
+++ fibonacci series.py
@@ -4,12 +4,12 @@ def calc_fib(n):
     if n<=1 :
         return n
     else:
-        fib = [None]*n
+        fib = [None]*(n+1)
         fib[0] = 0
         fib[1] = 1
-        for i in range(2,n):
+        for i in range(2,n+1):
             fib[i] = fib[i-1] + fib[i-2]
-    return fib[n-1]
+    return fib[n]
 
 n = int(input())
 print(calc_fib(n))
